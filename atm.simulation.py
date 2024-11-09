d700df452caad44c8e84a5ea53c4f0e522370843
Raghav490 <raghav.shakya445@gmail.com>
2024-11-09 16:49:47 +0530

    Add files via upload

---
+++ atm.simulation.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+print(f"hello{name}")
+print("how may i help you","\n")
+message="""please select any of the option"""
+##withdrawl
+# #deposite
+# type 1= check balance      
+print=(message,"\n")
